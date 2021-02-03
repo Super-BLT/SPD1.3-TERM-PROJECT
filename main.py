06c10f22f9c2d33846ec303b5783852f5aa2b934
Shaun Norton <shaunnorton87@gmail.com>
2021-02-03 12:13:25 -0800

    Working on a completly unnessary feature for the fun of it

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, make_response,send_file, render_template, request
 from dotenv import load_dotenv
 import os
 
@@ -11,7 +11,23 @@ app = Flask('__main__')
 
 @app.route("/")
 def homepage():
-    return "<h1>Welcome to uText! This project is a work in progress please check back later to experience the complete product.</h1>"
+    return render_template('temp.html')
 
+@app.route("/save/<url>",methods=["GET","POST"])
+def saveurl(url):
+    """Save Transcript Locally"""
+    #makeApi Call with Url
+    
+    filename=f'{url}Transcript.txt'
+    print(filename)
+    if request.method == 'POST':
+        new_file = open(filename,"x")
+        new_file.write('Contents of API Call')
+        new_file.close()
+        return send_file(filename, as_attachment=True), os.remove(filename)
+    
+        
+
+    return render_template('temp.html')
 if __name__ == "__main__":
     app.run(debug=True)

19e87460ea06a502173390edc25b0ab07fc05bc9
Shaun Norton <shaunnorton87@gmail.com>
2021-02-08 13:04:59 -0800

    homepage route now renders the index.html template.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from dotenv import load_dotenv
 import os
 
@@ -11,7 +11,7 @@ app = Flask('__main__')
 
 @app.route("/")
 def homepage():
-    return "<h1>Welcome to uText! This project is a work in progress please check back later to experience the complete product.</h1>"
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True)

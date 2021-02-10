701ba7f019bb919325ea3667b9746ca83ba8d401
Shaun Norton <shaunnorton87@gmail.com>
2021-02-10 11:13:17 -0800

    The videoid is not retrevied from the url

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request
 from dotenv import load_dotenv
 import os
 
@@ -11,6 +11,13 @@ app = Flask('__main__')
 
 @app.route("/")
 def homepage():
+    url = request.args.get("video-url")
+    video_code = ''
+    if url:
+        split_url = url.split("?v=")
+        video_code = split_url[1]
+        
+    
     return render_template("index.html")
 
 if __name__ == "__main__":
